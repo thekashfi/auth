9304bb75873c0c20517e30713e3493a06bd0562f
thekashfi <alikashfi12345@gmail.com>
2022-07-02 11:59:55 +0430

    --amend

--- app/database/DatabaseFactory.php
+++ app/database/DatabaseFactory.php
@@ -13,6 +13,12 @@ class DatabaseFactory
         $class = "App\\Database\\$name\\$name";
         if (file_exists(ROOT . "/app/database/{$type}/{$name}.php") &&
             class_exists($class)) {
+            $driver = new $class;
+
+            // Interface check
+            if (! $driver instanceof DriverInterface) {
+                die("database driver '{$type}' doesn't implement DriverInterface!");
+            }
             return new $class;
         }
         die("database driver '{$type}' is not installed!");
